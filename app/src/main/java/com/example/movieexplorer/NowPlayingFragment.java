75110bc0d2b91707ae26d4f72b9d666556ff14cd
Emmanuelikeh <88993873+Emmanuelikeh@users.noreply.github.com>
2022-10-03 13:07:57 -0500

    migrated to fragments commit

---
+++ app/src/main/java/com/example/movieexplorer/NowPlayingFragment.java
@@ -0,0 +1,124 @@
+package com.example.movieexplorer;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.movieexplorer.adapters.MovieAdapter;
+import com.example.movieexplorer.databinding.FragmentNowPlayingBinding;
+import com.example.movieexplorer.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class NowPlayingFragment extends Fragment {
+    FragmentNowPlayingBinding fragmentNowPlayingBinding;
+    public static final String NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    List<Movie> movies;
+    private EndlessRecyclerViewScrollListener scrollListener;
+
+
+    public NowPlayingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+        fragmentNowPlayingBinding = FragmentNowPlayingBinding.inflate(inflater,container,false);
+        return fragmentNowPlayingBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        movies = new ArrayList<>();
+        MovieAdapter movieAdapter = new MovieAdapter(getContext(), movies);
+        fragmentNowPlayingBinding.rvMovies.setAdapter(movieAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        fragmentNowPlayingBinding.rvMovies.setLayoutManager(linearLayoutManager);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadNextDataFromApi(page, movieAdapter);
+                    }
+                }, 1000); // Millisecond 1000 = 1 sec
+            }
+        };
+
+        fragmentNowPlayingBinding.rvMovies.addOnScrollListener(scrollListener);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "Success");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+            }
+        });
+    }
+
+
+    private void loadNextDataFromApi(int page, MovieAdapter movieAdapter) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("page",page);
+        client.get(NOW_PLAYING,params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "Success");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+}
